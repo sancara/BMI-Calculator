4a84e270c8f8590f045e63e967f6e0f9715d6db2
Sancara <santiagocaraballo@gmail.com>
2022-01-22 17:20:37 -0300

    setting UI from code

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Santiago Caraballo on 22/01/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.text = "Hello"
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+    }
+}
